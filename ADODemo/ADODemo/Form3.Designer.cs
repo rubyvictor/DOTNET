78eeb8a9b025b81cf2de63639cd1ceb7b5445963
Victor Lee <myleevictor@gmail.com>
2017-09-19 10:47:09 +0800

    data set practice

--- ADODemo/ADODemo/Form3.Designer.cs
+++ ADODemo/ADODemo/Form3.Designer.cs
@@ -32,6 +32,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -70,11 +73,43 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Rating";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(361, 317);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "DataSet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DataSetGet);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(87, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.insert);
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(552, 382);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -92,5 +127,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- ADODemo/ADODemo/Form3.cs
+++ ADODemo/ADODemo/Form3.cs
@@ -9,10 +9,16 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
+
 namespace ADODemo
 {
     public partial class Form3 : Form
     {
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        SqlCommandBuilder cmb;
+        DataSet ds;
+
         public Form3()
         {
             InitializeComponent();
@@ -57,9 +63,81 @@ namespace ADODemo
 
         }
 
+        
+
+
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             MessageBox.Show(listBox1.SelectedItem.ToString());
+        }
+
+        public static void DatasetStructure()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public static void DisplayData()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+
+        private void DataSetGet(object sender, EventArgs e)
+        {
+            //NorthWind Example 1.3
+
+            string connectionStr = "data source=(local);integrated security=SSPI;initial catalog=NorthWind;";
+            SqlConnection connect = new SqlConnection(connectionStr);
+
+            String commandText = "Select CompanyName from Customers";
+
+            cmd = new SqlCommand(commandText, connect);
+            da = new SqlDataAdapter(cmd);
+            cmb = new SqlCommandBuilder(da);
+
+            label2.Text = cmb.GetInsertCommand().CommandText;
+
+            ds = new DataSet();
+            connect.Open();
+            da.Fill(ds, "Customers");//give a name Customers of type String to the Data Table.  DataTable inside Dataset
+
+            //listBox1.Items.Add(da.Fill(ds,"Customers"));
+            //listBox1.Items.Add(ds.Tables.Count);
+            //listBox1.Items.Add(ds.Tables[0].Columns.Count.ToString());
+            //listBox1.Items.Add(ds.Tables[0].Columns[1].ColumnName.ToString());
+            ////pick column, pick row, like a 2D array
+            //listBox1.Items.Add(ds.Tables[0].Rows[0][1].ToString());
+            ////Less confusing
+            //listBox1.Items.Add(ds.Tables["Customers"].Rows[4]["CompanyName"].ToString());
+            listBox1.Items.Add(ds.Tables["Customers"].Rows[2].ToString());
+            
+            //Save data to Database
+            //da.Update(ds, "Customers");
+            // UPDATE Customers
+            // SET CustomerID = '007'
+            // WHERE CompanyName='Alfreds Futterkiste';
+            connect.Close();
+            
+
+
+        }
+
+        private void insert(object sender, EventArgs e)
+        {
+            DataRow r = ds.Tables["Customers"].NewRow();
+            r["CustomerID"] = "777";
+            r["CompanyName"] = "ViktorLi";
+
+            ds.Tables["Customers"].Rows.Add(r);
+            label2.Text = ds.Tables[0].Rows.Count.ToString();
+
+            da.Update(ds, "Customers");
+
         }
     }
 }
